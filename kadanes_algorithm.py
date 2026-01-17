87bb05529d60fa4b06cb78311de69c3f97ff59e8
Anastasiya <aadworkmaill@gmail.com>
2026-01-17 20:08:05 +0300

    Create kadanes_algorithm.py

---
+++ kadanes_algorithm.py
@@ -0,0 +1,17 @@
+def maxSubArray(nums):
+
+    current_max = nums[0]
+    global_max = nums[0]
+
+    for num in nums[1:]:
+
+        current_max = max(num, num+current_max)
+
+        if current_max > global_max:
+
+            global_max = current_max
+
+    return global_max
+
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+print(maxSubArray(nums))
