733c392619760ecfa5d0d0bd928ce4248181f755
Anastasiya <aadworkmaill@gmail.com>
2026-04-04 14:28:12 +0300

    fix kadanes algorithm

--- search/kadanes_algorithm.py
+++ search/kadanes_algorithm.py
@@ -1,17 +1,16 @@
-def maxSubArray(nums):
+numbers = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 
-    current_max = nums[0]
-    global_max = nums[0]
+def kadanes_algorithm(numbers):
+    current_max = numbers[0] # текущее максимальное подмассивов, заканчивающихся на текущей позиции
+    global_max = numbers[0] # самая большая сумма из всех подмассивов
 
-    for num in nums[1:]:
-
-        current_max = max(num, num+current_max)
+    # начинаем проверять со второго элемента в массиве
+    for num in numbers[1:]:
+        current_max = max(num, num+current_max) # выбираем - выгоднее начать новый подмассив или добавить в текущий
 
         if current_max > global_max:
-
             global_max = current_max
 
     return global_max
 
-nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-print(maxSubArray(nums))
+print(kadanes_algorithm(numbers))
